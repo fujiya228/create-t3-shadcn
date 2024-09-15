6d478dd4b4cc188541c3cbad328722ba52b06fb6
fujiya228 <fujiya0xE2@gmail.com>
2024-09-16 03:28:43 +0900

    disable stdio

--- cli/src/utils/run-command.ts
+++ cli/src/utils/run-command.ts
@@ -2,7 +2,7 @@ import { execSync } from 'child_process';
 
 export const runCommand = (command: string, cwd?: string) => {
   try {
-    execSync(command, { stdio: 'inherit', cwd: cwd || process.cwd() });
+    execSync(command, { cwd: cwd || process.cwd() });
   } catch (error) {
     if (error instanceof Error) {
       console.error(`\nFailed to execute: ${command}\n\n${error.message}\n`);
